5f3a252bbf81a91119d4260d0bf7fa6415e06e87
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-02-02 16:30:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // ============ GLOBAL VARIABELS ============ //
-const endpoint = ""; // To do: paste url to endpoint
+const endpoint =
+  "https://race-dat-v1-default-rtdb.europe-west1.firebasedatabase.app"; // To do: paste url to endpoint
 let selectedUser;
 
 // ============ READ ============ //
@@ -18,42 +19,78 @@ function displayUsers(list) {
     //loop through all users and create an article with content for each
     for (const user of list) {
         document.querySelector("#users-grid").insertAdjacentHTML(
-            "beforeend",
-            /*html*/ `
+          "beforeend",
+          /*html*/ `
             <article>
                 <h2>${user.name}</h2>
+                <img src="${user.image}">
+                <p>${user.title}</p>
+                <a href="mailto:${user.mail}">${user.mail}</a>
+                <div class="btns">
+		<button class="btn-update-user">Update</button>
+</div>
             </article>
         `
         );
         // To do: Add event listeners
+    
     }
 }
 
 // ============ CREATE ============ //
 // Create (POST) user to Firebase (Database) using REST API
 async function createUser(event) {
-    event.preventDefault();
+  event.preventDefault();
+  console.log("Opret bruger");
+  const name = event.target.name.value;
+  const title = event.target.title.value;
+  const mail = event.target.mail.value;
+  const image = event.target.image.value;
+  // To do: add variables with reference to input fields (event.target.xxxx.value)
 
-    // To do: add variables with reference to input fields (event.target.xxxx.value)
+  // create a new user
+  const newUser = { name, title, mail, image }; // To do: add all fields/ variabels
+  const userAsJson = JSON.stringify(newUser);
+  const response = await fetch(`${endpoint}/users.json`, {
+    method: "POST",
+    body: userAsJson,
+  });
 
-    // create a new user
-    const newUser = {}; // To do: add all fields/ variabels
-    const userAsJson = JSON.stringify(newUser);
-    const response = await fetch(`${endpoint}/users.json`, {
-        method: "POST",
-        body: userAsJson
-    });
-
-    if (response.ok) {
-        // if success, update the users grid
-        // To do: make sure to update the users grid in order to display the new user
-        // and scroll to top
-        // To do: call scrollToTop to scroll when created
-    }
+  if (response.ok) updateUsersGrid();
+  // if success, update the users grid
+  // To do: make sure to update the users grid in order to display the new user
+  // and scroll to top
+  // To do: call scrollToTop to scroll when created
+  // and scroll to top
+  scrollToTop();
 }
 
+
 // ============ UPDATE ============ //
-function selectUser(user) {
+function selectUser(user) {function displayUsers(list) {
+  // reset <section id="users-grid" class="grid-container">...</section>
+  document.querySelector("#users-grid").innerHTML = "";
+  //loop through all users and create an article with content for each
+  for (const user of list) {
+    document.querySelector("#users-grid").insertAdjacentHTML(
+      "beforeend",
+      /*html*/ `
+            <article>
+                <img src="${user.image}">
+                <h2>${user.name}</h2>
+                <p>${user.title}</p>
+                <a href="mailto:${user.mail}">${user.mail}</a>
+                 <div class="btns">
+                    <button class="btn-update-user">Update</button>
+                </div>
+            </article>
+        `
+    );
+    document
+      .querySelector("#users-grid article:last-child .btn-update-user")
+      .addEventListener("click", () => selectUser(user));
+  }
+}
     // Set global varaiable
     selectedUser = user;
     // reference to update form
@@ -99,6 +136,8 @@ async function deleteUser(id) {
 // To do: add submit event listener to create form (#form-create)
 // To do: add submit event listener to update form (#form-update)
 
+document.querySelector("#form-create").addEventListener("submit", createUser);
+
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: "smooth" });
 }
@@ -106,7 +145,9 @@ function scrollToTop() {
 async function updateUsersGrid() {
     const users = await readUsers();
     displayUsers(users);
+    
 }
 
 // ============ Init CRUD App ============ //
 // To do: call/ run updateUsersGrid to initialise the app
+updateUsersGrid();
